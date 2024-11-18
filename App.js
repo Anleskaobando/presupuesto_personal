116cb01590cd2e62fe2cae0bd88956c5b5e0b60c
Anleskaobando <anleskaobando871@gmail.com>
2024-11-17 21:30:51 -0600

    Añadiendo el Inicializador de Fuentes al Iniciar la App

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
-import "react-native-gesture-handler"; // Necesario para react-navigation
+import "react-native-gesture-handler";
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
-import StackNavigator from "./navigation/StackNavigator"; // Ajusta la ruta si es necesario
+import StackNavigator from "./navigation/StackNavigator";
+import AppInitializer from "./AppInitializer";
 
 export default function App() {
   return (
     <NavigationContainer>
-      <StackNavigator />
+      <AppInitializer />
     </NavigationContainer>
   );
 }
